2af86960f8aea97f1018555eb620c245e0a8fc85
Gabriel José <thirdendeal@protonmail.com>
2022-02-14 12:40:23 -0300

    Refactor proceeding logic

--- src/kattis/different/different.rb
+++ src/kattis/different/different.rb
@@ -2,13 +2,13 @@
 #
 # https://open.kattis.com/problems/different
 
-ARGF.each do |line|
-  match =
+ARGF.each_line do |line|
+  numbers =
     line
     .scan(/[[:digit:]]+/)
+    .map(&:to_i)
 
-  difference =
-    match[0].to_i - match[1].to_i
+  difference = numbers[0] - numbers[1]
 
   puts(difference.abs)
 end
