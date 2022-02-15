37a38fb96816c5a0a6b4c6bf6543d06637ec82c2
Gabriel José <thirdendeal@protonmail.com>
2022-02-15 17:31:27 -0300

    Solve SPOJ's problem 'prime1'

---
+++ src/spoj/prime1/prime1.rb
@@ -0,0 +1,29 @@
+# PRIME1 - Prime Generator
+#
+# https://www.spoj.com/problems/PRIME1/
+
+require 'prime'
+require 'stringio'
+
+head = ARGF.first
+body = ARGF.first(head.to_i)
+
+to_puts = body.map do |line|
+  lower, upper =
+    line
+    .scan(/[[:digit:]]+/)
+    .map(&:to_i)
+
+  generator = Prime.each(upper)
+
+  generator.grep(lower..upper)
+end
+
+io = StringIO.new
+
+to_puts.each do |array|
+  io.puts(array)
+  io.puts
+end
+
+puts(io.string.chomp)
