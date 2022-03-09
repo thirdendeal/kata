68039d1530e1a170af8f1942f861ed4927dd0b3f
Gabriel José <thirdendeal@protonmail.com>
2022-03-09 00:04:31 -0300

    Add piped input support

--- test/all.rb
+++ test/all.rb
@@ -1,7 +1,20 @@
 # All Test
 
+stdin = ARGF.read unless STDIN.tty?
+
+if stdin
+  stdin = stdin.each_line.map do |line|
+    File.absolute_path(line.chomp)
+  end
+end
+
 exit_status = Dir.chdir(__dir__) do
-  solutions = Dir['../src/*/*']
+  solutions =
+    if String(stdin).empty?
+      Dir['../src/*/*']
+    else
+      stdin
+    end
 
   solutions.each_slice(4).all? do |batch|
     batch.map! do |solution|
