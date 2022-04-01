f01a05e292d3e895aa4832f083096eac2fd9f166
Gabriel José <thirdendeal@protonmail.com>
2022-04-01 10:40:09 -0300

    Solve Project Euler's problem 6

---
+++ src/project-euler/6.rb
@@ -0,0 +1,11 @@
+# Sum square difference
+#
+# https://projecteuler.net/problem=6
+
+numbers = 1.upto(100)
+squares = numbers.map { |n| n**2 }
+
+difference =
+  numbers.sum**2 - squares.sum
+
+puts(difference) # => 25164150
