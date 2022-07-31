679ed6c0672f6b04a7987ce888ed738e5a088048
Gabriel José <thirdendeal@protonmail.com>
2022-07-30 23:48:11 -0300

    Solve Kattis' problem 'dominos'

---
+++ src/kattis/3-4/dominos/dominos.rb
@@ -0,0 +1,43 @@
+# Dominos
+#
+# https://open.kattis.com/problems/dominos
+
+module Input
+  extend self
+
+  def cases(lines)
+    Enumerator.new do |yielder|
+      case_number = lines.shift.to_i
+
+      case_number.times do
+        dominos, mapping_number =
+          numbers(lines.shift)
+
+        mappings = mapping_number.times.map do
+          numbers(lines.shift)
+        end
+
+        yielder << [dominos, mappings]
+      end
+    end
+  end
+
+  private
+
+  def numbers(string)
+    string
+      .scan(/[[:digit:]]+/)
+      .map(&:to_i)
+  end
+end
+
+cases = Input.cases(
+  ARGF.readlines
+)
+
+cases.each do |dominos, mappings|
+  knocked =
+    mappings.map(&:last)
+
+  puts(dominos - knocked.size)
+end
